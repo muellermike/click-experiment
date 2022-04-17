7cbf63580e56bd343e300f0e34d1af0832b4474c
muellermike <miki.mueller@hotmail.com>
2022-04-17 16:59:10 +0200

    use react-bootstrap and create first "layout"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/pages/exercise/Exercise.js
+++ src/pages/exercise/Exercise.js
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
+import { Accordion, Button, Card, Col, Container, Form, Row, Spinner } from "react-bootstrap";
 
 function Exercise() {
-    let daten = [];
+    let daten = [
+        { question: "Which side has more dots?", img: ""}
+    ];
 
     // lade Daten von einem API
     useEffect(() => {
@@ -11,10 +14,67 @@ function Exercise() {
 
     // Daten anzeigen
     return (
-        //TODO: add react bootstrap
         <div>
-            Das sollen die Daten sein:
-            {daten}
+            <Container>
+                <Row className="Container-Row">
+                    <Col xs={5}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Text>
+                                    {daten[0].question}
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Img variant="bottom" src={daten[0].img} />
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Answer</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your voice inputwith anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" disabled type="submit">
+                                <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                                />
+                                Loading...
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>Experiment Description</Accordion.Header>
+                                <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                est laborum.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                </Row>
+            </Container>
+            
         </div>
     )
 }
