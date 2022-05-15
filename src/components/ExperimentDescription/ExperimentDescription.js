29b55ed5189182b28f48496794c7161a00589bed
muellermike <miki.mueller@hotmail.com>
2022-05-15 13:34:56 +0200

    change experiment description

--- src/components/ExperimentDescription/ExperimentDescription.js
+++ src/components/ExperimentDescription/ExperimentDescription.js
@@ -9,13 +9,11 @@ function ExperimentDescription() {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Experiment Description</Accordion.Header>
                     <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                    est laborum.
+                    With the Dots Game we try to train a model which understands a user answer provided by voice.
+                    Therefore, please answer in a sentence such as "There are more dots on the right / left side".
+                    Provide either right or left depending on the Dots Game. Note that we are going to pay according to your
+                    mentioned result and not whether the result was correct.
+                    In other words, when the true result was left but you stated right we pay you the amount for the answer right.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
