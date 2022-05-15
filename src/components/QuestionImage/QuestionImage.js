9925c072056b889331265ba6b92abd3a1a29df14
muellermike <miki.mueller@hotmail.com>
2022-05-15 14:25:24 +0200

    show image only 2secs

--- src/components/QuestionImage/QuestionImage.js
+++ src/components/QuestionImage/QuestionImage.js
@@ -3,9 +3,52 @@ import "./QuestionImage.css";
 import { Card } from "react-bootstrap";
 
 class QuestionImage extends React.Component {
+    constructor(props){
+        super(props);
+
+        // set basic state values
+        this.state = {
+            timeIsUp: false
+        };
+    }
+
+    /*
+    *   Run steps after component is mounted:
+    *   set timeout for showing image several seconds
+    */
+     componentDidUpdate(prevProps, prevState) {
+         if(prevProps !== this.props) {
+            this.setState({
+                timeIsUp: false
+            });
+            
+            this.timer = setTimeout(() => {
+                this.setState({
+                    timeIsUp: true
+                   });
+               }, 2000);
+         }
+    }
+
+    componentDidMount() {
+        this.timer = setTimeout(() => {
+            this.setState({
+                timeIsUp: true
+               });
+           }, 2000);
+    }
+
+    /*
+    *  Run steps before the component will unmount.
+    *  timeout is cleared.
+    */
+    componentWillUnmount() {
+        if(this.timer) {
+            clearTimeout(this.timer);
+        }
+    }
 
     render(){
-        //TODO: implement logic for showing the picture only one second: https://stackoverflow.com/questions/54958440/how-to-make-the-background-image-change-every-x-seconds-in-react
         // show picture and question
         return (
             <div>
@@ -16,7 +59,9 @@ class QuestionImage extends React.Component {
                         </Card.Text>
                     </Card.Body>
                     <div>
-                        <Card.Img className="Image-Dots" variant="bottom" src={this.props.image} />
+                        {this.state.timeIsUp ? <Card.Img className="Image-Dots" variant="bottom" /> : 
+                            <Card.Img className="Image-Dots" variant="bottom" src={this.props.image} />
+                        }
                     </div>
                 </Card>
             </div>
