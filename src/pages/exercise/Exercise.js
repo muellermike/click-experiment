165d8ec8a44e281e897dd5749256221ee355f6e9
muellermike <mueller.doublem@gmail.com>
2022-07-04 10:55:45 +0200

    send time to exercise answer

--- src/pages/exercise/Exercise.js
+++ src/pages/exercise/Exercise.js
@@ -54,7 +54,7 @@ function Exercise() {
                 mode: 'cors',
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json', 'X-API-KEY': process.env.REACT_APP_API_KEY_VALUE },
-                body: JSON.stringify({ answer: answer, userId: globalState.userId, experimentId: parseInt(experimentId), exerciseId: parseInt(exercise.id) })
+                body: JSON.stringify({ answer: answer, userId: globalState.userId, time: new Date().toISOString(), experimentId: parseInt(experimentId), exerciseId: parseInt(exercise.id) })
             };
             fetch(process.env.REACT_APP_API_BASE_URL + '/exercises', requestOptions)
             .then(response => {
