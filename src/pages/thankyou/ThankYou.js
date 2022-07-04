fee7d71fcbc9892732f7452e12cfe637478acc9c
muellermike <mueller.doublem@gmail.com>
2022-07-04 16:05:14 +0200

    change query param for unipark url

--- src/pages/thankyou/ThankYou.js
+++ src/pages/thankyou/ThankYou.js
@@ -55,7 +55,7 @@ function ThankYou() {
                                         You provided the ID: {globalState.externalUserId}.
                                         <b>Please go back to Uni-Park with the button below and answer the concluding questions about the experiment</b>. Thank you.
                                     </Card.Text>
-                                    <Button variant="primary" href={process.env.REACT_APP_UNIPARK_RET_LINK + globalState.externalUserId}>Go back to Uni-Park</Button>
+                                    <Button variant="primary" href={process.env.REACT_APP_UNIPARK_RET_LINK + "?return_tic=" + globalState.externalUserId}>Go back to Uni-Park</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
