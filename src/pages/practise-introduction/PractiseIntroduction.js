10713d582bd5fbf3a963fa85d73b85dead5385f6
muellermike <mueller.doublem@gmail.com>
2022-07-14 16:02:22 +0200

    shorten practice intro text

--- src/pages/practise-introduction/PractiseIntroduction.js
+++ src/pages/practise-introduction/PractiseIntroduction.js
@@ -1,13 +1,10 @@
 import "./PractiseIntroduction.css";
 import { Card, Container, Row, Button, ListGroup } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from 'react-redux';
 import ImportantInformation from "../../components/ImportantInformation/ImportantInformation";
 
 function PractiseIntroduction() {
     let navigate = useNavigate();
-
-    const imageState = useSelector(state => state.imageState);
 
     const handleClickNext = (event) => {
         event.preventDefault();
@@ -28,51 +25,32 @@ function PractiseIntroduction() {
                     </Row>
                     <Row>
                         <Card>
-                            <Card.Title>Dots Game</Card.Title>
-                            <Card.Body className="card-body-practise-intro">
-                                <p>In the next step you have the chance to test the experiment by playing two practice rounds. You will see the dots game almost as it is during the experiemnt. The only difference is that there are more hints how the user interface works. Take your time to figure out how the user interface works and how you can interact with it. The practice game does not influence the payment.</p>
-                                <p>You have to solve the task by answering whether there are more dots on the right or left side. Therefore, please <b>click on the corresponding button which says "left" / "right"</b>. Note that we are going to pay according to your mentioned result and not whether the result was correct.</p>
-                                <p>To solve the game, note the steps below.</p>
-                                <ListGroup as="ol" numbered>
-                                    <ListGroup.Item
-                                        as="li"
-                                        className="d-flex justify-content-between align-items-start"
-                                    >
-                                        <div className="ms-2 me-auto">
-                                        <div className="fw-bold">Dots Image on the left side</div>
-                                        There is a dots image shown on the left side. Look at it first, as <b>it is only shown {imageState.imageTime / 1000} seconds</b>. Make sure to look at the image and decide what to answer. After this time, it is replaced with an empty image.
-                                        </div>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item
-                                        as="li"
-                                        className="d-flex justify-content-between align-items-start"
-                                    >
-                                        <div className="ms-2 me-auto">
-                                        <div className="fw-bold">Select your answer</div>
-                                        On the right side of the desktop view, you see the answer opportunity. Click on the button with the answer you want to provide. On mobile, the answer buttons are shown below the image.
-                                        </div>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item
-                                        as="li"
-                                        className="d-flex justify-content-between align-items-start"
-                                    >
-                                        <div className="ms-2 me-auto">
-                                        <div className="fw-bold">Submit answer</div>
-                                        Once you have selected your answer, you can submit the answer <b>by clicking on the blue button on the bottom.</b>
-                                        </div>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item
-                                        as="li"
-                                        className="d-flex justify-content-between align-items-start"
-                                    >
-                                        <div className="ms-2 me-auto">
-                                        <div className="fw-bold">Next round</div>
-                                        <b>After clicking the submit button, the next exercise is shown immediately</b>. Start again with step 1.
-                                        </div>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                                <br /><p>Once you are ready, <b>scroll down to start the experiment</b>.</p>
-                                <Button variant="primary" onClick={handleClickNext} >Go to the practise game</Button>
+                            <Card.Title>Dots Estimation Experiment</Card.Title>
+                            <Card.Body>
+                                <div>
+                                    <div className="card-body-practise-intro">
+                                        <ListGroup as="ul" variant="flush">
+                                            <ListGroup.Item
+                                                as="li"
+                                            >
+                                                Following you will have the chance to test the experiment by playing two practice rounds.
+                                            </ListGroup.Item>
+                                            <ListGroup.Item
+                                                as="li"
+                                            >
+                                                Take your time to figure out how the user interface works and how you can interact with it.
+                                            </ListGroup.Item>
+                                            <ListGroup.Item
+                                                as="li"
+                                            >
+                                                The practice game does not influence the payment.
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </div>
+                                    <div style={{align: "center", width: "100%"}}>
+                                        <Button variant="primary" onClick={handleClickNext} >Go to the practise game</Button>
+                                    </div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Row>
